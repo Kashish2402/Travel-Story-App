6d9100c26b0349d88f65cd7b7ae4b1c72d3bee27
Kashish2402 <68956193+Kashish2402@users.noreply.github.com>
2025-02-27 14:52:18 +0530

    Update user.models.js

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -16,13 +16,14 @@ const userSchema = new Schema(
     username: {
       type: String,
       required: true,
+      lowercase:true
     },
     password: {
       type: String,
       required: true,
     },
     dateOfBirth: {
-      type: Number,
+      type: Date,
       required: true,
     },
     gender: {
