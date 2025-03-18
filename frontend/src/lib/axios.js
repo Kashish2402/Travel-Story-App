a763c4f9129853e7e9a03b965dd40b84de7d9043
Kashish2402 <68956193+Kashish2402@users.noreply.github.com>
2025-03-18 22:22:03 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
-import axios from "axios"
+import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api/v1"; 
 
-export const axiosInstance=axios.create({
-    baseURL:"http://localhost:5000/api/v1",
-    withCredentials:true
-})
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true, 
+});
