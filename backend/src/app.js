8975df478e5f56ba804011b93a92a308628993dd
Kashish2402 <Kashishgupta2402@gmail.com>
2025-03-18 19:34:45 +0530

    setting up for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { errorHandler } from "./middlewares/errorHandler.middleware.js";
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express();
+
+const _dirname = path.resolve();
 
 app.use(
   cors({
@@ -19,13 +22,18 @@ app.use(cookieParser());
 
 // ROUTES
 
-import userRoutes from './routes/user.routes.js'
-import storyRoutes from './routes/travelStory.routes.js'
-import likeRoutes from "./routes/like.routes.js"
+import userRoutes from "./routes/user.routes.js";
+import storyRoutes from "./routes/travelStory.routes.js";
+import likeRoutes from "./routes/like.routes.js";
 
-app.use("/api/v1/users",userRoutes)
-app.use("/api/v1/travelStory",storyRoutes)
-app.use("/api/v1/likes",likeRoutes)
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/travelStory", storyRoutes);
+app.use("/api/v1/likes", likeRoutes);
 
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+});
 app.use(errorHandler);
 export { app };
