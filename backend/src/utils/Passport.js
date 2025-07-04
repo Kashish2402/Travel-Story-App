1044d8466957c071688809d66b5a1982f8ad12d4
Kashish2402 <68956193+Kashish2402@users.noreply.github.com>
2025-07-04 11:16:53 +0000

    update url

--- backend/src/utils/Passport.js
+++ backend/src/utils/Passport.js
@@ -7,7 +7,7 @@ dotenv.config()
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/v1/auth/google/callback"
+    callbackURL: "https://travel-story-app-t1sb.onrender.com//api/v1/auth/google/callback"
 },
 
     async (accessToken, refreshToken, profile, done) => {
